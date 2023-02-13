11f34df81df8145ecdad6d0dd6ba149329eeb272
vanthien64 <vanthiendiendien@gmail.com>
2023-02-13 17:04:49 +0700

    update new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Homepage } from './homepage.component';
+import { Product } from './product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // Homepage,
+    Product
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class Product {
+}
